6de496a3173f646106f3cfe9aeb977b8013b1619
Juliano Oliveira <jul.oliveira@gmail.com>
2023-07-18 16:04:28 -0300

    Create masstransit-filter-issue.cs

---
+++ masstransit-filter-issue.cs
@@ -0,0 +1,52 @@
+using MassTransit;
+using MassTransit.Issues;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMassTransit(bus =>
+{
+    bus.AddConsumer<MyConsumer>();
+    bus.UsingInMemory((context, cfg) =>
+    {
+        cfg.ReceiveEndpoint("my-queue", e =>
+        {
+            e.UseConsumeFilter<MyConsumerFilter>(context);
+            e.ConfigureConsumer<MyConsumer>(context);
+        });
+    });
+});
+
+var app = builder.Build();
+
+app.MapGet("/send-message", async (IPublishEndpoint publishEndpoint) =>
+{
+    await publishEndpoint.Publish<MyMessage>(new { Message = "TEST MESSAGE" });
+});
+
+app.Run();
+
+namespace MassTransit.Issues
+{
+    public class MyMessage
+    {
+        public string? Message { get; set; }
+    }
+
+    public class MyConsumer : IConsumer<MyMessage>
+    {
+        public Task Consume(ConsumeContext<MyMessage> context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+    public class MyConsumerFilter : IFilter<ConsumeContext<MyMessage>>
+    {
+        public void Probe(ProbeContext context) { }
+
+        public async Task Send(ConsumeContext<MyMessage> context, IPipe<ConsumeContext<MyMessage>> next)
+        {
+            await next.Send(context);
+        }
+    }
+}
